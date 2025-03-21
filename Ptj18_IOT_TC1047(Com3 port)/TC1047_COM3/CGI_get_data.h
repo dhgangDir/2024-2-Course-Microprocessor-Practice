cd1ec623bb6b5b95f99e245c1494c0ec6ee35a6a
dhgangDir <108117157+dhgangDir@users.noreply.github.com>
2025-03-21 09:50:55 +0900

    Add files via upload

---
+++ Ptj18_IOT_TC1047(Com3 port)/TC1047_COM3/CGI_get_data.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#ifndef __CGI_GET_DATA_H__
+#define __CGI_GET_DATA_H__
+
+#define MAX_N_HEAD 100
+#define DFL_N_HEAD 10
+
+#define MAX_N_TAIL 100
+#define DFL_N_TAIL 10
+
+#define CHR_P_LINE 80
+
+void CGI_get_data(char* port, char* cmd, int nHeadLine, int nTailLine, char* data);
+
+#endif
---
+++ Ptj18_IOT_TC1047(Com3 port)/TC1047_COM3/TC1047_COM3.c
@@ -0,0 +1,20 @@
+﻿// TC1047_COM3.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <stdlib.h>
+#include "TC1047_COM3_web.h"
+#include "CGI_get_data.h"
+
+int main()
+{
+    char* data;
+
+    data = (char*)malloc((DFL_N_HEAD + DFL_N_TAIL) * CHR_P_LINE);
+
+    CGI_get_data("COM3", "tc1047", DFL_N_HEAD, DFL_N_TAIL, data);
+    TC1047_CGI_web(data);
+
+    
+    return 0;
+}
+
